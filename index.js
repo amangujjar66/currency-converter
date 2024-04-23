0422d35a6db5719ed03090a6c1d2a6a80d154d03
amangujjar66 <166032720+amangujjar66@users.noreply.github.com>
2024-04-23 13:11:51 -0700

    first commit

---
+++ index.js
@@ -0,0 +1,34 @@
+import inquirer from "inquirer";
+import chalk from "chalk";
+const currency = {
+    USD: 1,
+    EUR: 0.91,
+    GBP: 0.76,
+    INR: 74.57,
+    PKR: 280
+};
+let user_answer = await inquirer.prompt([
+    {
+        name: "from",
+        message: chalk.cyanBright("Enter From Currency"),
+        type: "list",
+        choices: ['USD', 'EUR', 'GBP', 'INR', 'PKR',],
+    },
+    {
+        name: "to",
+        message: chalk.cyanBright("Enter To Currenct"),
+        type: "list",
+        choices: ['USD', 'EUR', 'GBP', 'INR', 'PKR',],
+    },
+    {
+        name: "amount",
+        message: chalk.cyanBright("Enter Your Amount"),
+        type: 'number',
+    }
+]);
+let fromAmount = currency[user_answer.from]; //exchange rate
+let toAmount = currency[user_answer.to]; //exchange rate
+let amount = user_answer.amount;
+let baseAmount = amount / fromAmount; //USD base currency 
+let convertedAmount = baseAmount * toAmount;
+console.log(chalk.blueBright(convertedAmount));
